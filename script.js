2401023c587d6af23500a1a0c6636210fe6bf224
sambarhino <ramon@blacktechnicalstudios.com>
2026-01-02 19:11:50 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+js
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+document.querySelectorAll(".scroll").forEach(el => observer.observe(el));
